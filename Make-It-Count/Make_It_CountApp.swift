23b2f69548b164d6ef6ee19b7cc7bcfb36706a20
Imelda Yoon <136655874+ParkBench78@users.noreply.github.com>
2023-12-17 17:10:33 -0800

    Initial Commit

---
+++ Make-It-Count/Make_It_CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Make_It_CountApp.swift
+//  Make-It-Count
+//
+//  Created by Imelda Yoon on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Make_It_CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
